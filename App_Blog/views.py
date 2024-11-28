8dee51fbe430d6ad3968e67a022c244c99564de3
NafisFuadTanvir <nafisf026@gmail.com>
2024-11-28 11:25:07 +0600

    fixing the nav bar and create the templates structure for the blog_app

--- App_Blog/views.py
+++ App_Blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
+from django.views.generic import CreateView,UpdateView,DeleteView,View,TemplateView,ListView,DetailView
+
 # Create your views here.
 
 def blog_list(request):
     
     return render(request,"App_Blog/blog_list.html",context={})
+
