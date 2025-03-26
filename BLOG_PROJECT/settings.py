9a07b457d6e12b6573aa6eee64b322abdb8e3bdf
NafisFuadTanvir <nafisf026@gmail.com>
2025-03-26 16:13:50 +0600

    config for vercel deployement

--- BLOG_PROJECT/settings.py
+++ BLOG_PROJECT/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-kf-t_yqi6l59nhfp(1ukkh^1z1bn-5e_px2ppldpi&vk($xuft
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
